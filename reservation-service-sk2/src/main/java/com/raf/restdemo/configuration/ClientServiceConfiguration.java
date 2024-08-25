42aa85a122a79e51c61699d139ae080f07b536c7
aleksandar234 <49760112+aleksandar234@users.noreply.github.com>
2024-08-25 13:49:58 +0200

    Update ClientServiceConfiguration.java

--- reservation-service-sk2/src/main/java/com/raf/restdemo/configuration/ClientServiceConfiguration.java
+++ reservation-service-sk2/src/main/java/com/raf/restdemo/configuration/ClientServiceConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
-
+//
 @Configuration
 public class ClientServiceConfiguration {
 
