4fd652c039830da10a3ac39446be6c42eb60133f
aleksandar234 <49760112+aleksandar234@users.noreply.github.com>
2024-08-25 13:50:48 +0200

    Update AdminController.java

--- rest-demo-master/src/main/java/com/raf/restdemo/controller/AdminController.java
+++ rest-demo-master/src/main/java/com/raf/restdemo/controller/AdminController.java
@@ -6,7 +6,7 @@ import com.raf.restdemo.service.AdminService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+//
 import javax.validation.Valid;
 
 @RestController
